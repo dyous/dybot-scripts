ae4575d336990e55cbafaaae69f88adad0b124cc
bouzuya <bouzuya@gmail.com>
2014-02-26 01:13:41 +0900

    improve todo

--- scripts/todo.js
+++ scripts/todo.js
@@ -8,9 +8,9 @@
 //   None
 //
 // commands:
-//   todo:        - Show tasks
-//   todo: <task> - Add task
-//   done: <idx>  - Remove task
+//   hubot todo        - Show tasks
+//   hubot todo <task> - Add task
+//   hubot done <idx>  - Remove task
 //
 // Author:
 //   bouzuya
@@ -20,30 +20,36 @@ module.exports = function(robot) {
 
   var BRAIN_KEY = 'todo';
 
-  robot.hear(/^todo:\s*$/i, function(res) {
-    var tasks = robot.brain.get(BRAIN_KEY) || [];
-    if (tasks.length === 0) {
-      res.send('todo: no item');
+  robot.respond(/todo\s*(.*)$/i, function(res) {
+    var user = res.envelope.user;
+    var data = robot.brain.get(BRAIN_KEY) || {};
+    var tasks = data[user.id] || [];
+    var task = res.match[1];
+    if (task) {
+      tasks.push(task);
+      data[user.id] = tasks;
+      robot.brain.set(BRAIN_KEY, data);
+      var idx = tasks.length - 1;
+      res.send('todo: [' + idx + '] "' + task + '" added');
     } else {
-      res.send('todo:\n' + tasks.map(function(task, idx) {
-        return ' [' + idx + '] "' + task + '"';
-      }).join('\n'));
+      if (tasks.length === 0) {
+        res.send('todo: no item');
+      } else {
+        res.send('todo:\n' + tasks.map(function(task, idx) {
+          return ' [' + idx + '] "' + task + '"';
+        }).join('\n'));
+      }
     }
   });
 
-  robot.hear(/^todo:\s+(.*)$/i, function(res) {
-    var task = res.match[1];
-    var tasks = robot.brain.get(BRAIN_KEY) || [];
-    tasks.push(task);
-    robot.brain.set(BRAIN_KEY, tasks);
-    var idx = tasks.length - 1;
-    res.send('todo: [' + idx + '] "' + task + '" added');
-  });
-
-  robot.hear(/^done:\s+(\d+)$/i, function(res) {
+  robot.respond(/done\s+(\d+)$/i, function(res) {
+    var user = res.envelope.user;
+    var data = robot.brain.get(BRAIN_KEY) || {};
     var idx = parseInt(res.match[1], 10);
-    var tasks = robot.brain.get(BRAIN_KEY) || [];
+    var tasks = data[user.id] || [];
     var removed = tasks.splice(idx, 1);
+    data[user.id] = tasks;
+    robot.brain.set(BRAIN_KEY, data);
     if (removed.length > 0) {
       var task = removed[0];
       res.send('todo: [' + idx + '] "' + task + '" removed');
